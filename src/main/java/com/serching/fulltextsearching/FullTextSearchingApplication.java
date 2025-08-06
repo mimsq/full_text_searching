6320b3630380d8a6dfc3b8d9f6ebd69f3de73e7c
xingmiao666888 <2764985740@qq.com>
2025-08-06 16:31:10 +0800

    项目初始化

---
+++ src/main/java/com/serching/fulltextsearching/FullTextSearchingApplication.java
@@ -0,0 +1,13 @@
+package com.serching.fulltextsearching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTextSearchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullTextSearchingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serching/fulltextsearching/FullTextSearchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serching.fulltextsearching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTextSearchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
