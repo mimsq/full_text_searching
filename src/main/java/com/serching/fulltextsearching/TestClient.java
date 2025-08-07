a9a16b1aaffbf89a8a069f31b2958aa4ef854441
xingmiao666888 <2764985740@qq.com>
2025-08-07 10:06:07 +0800

    修改了elasticsearch与spring的版本冲突问题

---
+++ src/main/java/com/serching/fulltextsearching/TestClient.java
@@ -0,0 +1,33 @@
+package com.serching.fulltextsearching;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.RestClient;
+
+
+public class TestClient {
+    public static void main(String[] args) {
+        // URL and API key
+        String serverUrl = "https://localhost:9200";
+
+// Create the low-level client
+        RestClient restClient = RestClient
+                .builder(HttpHost.create(serverUrl))
+                .setDefaultHeaders(new Header[]{
+                        new BasicHeader("Authorization", "ApiKey ")
+                })
+                .build();
+
+// Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+// And create the API client
+        ElasticsearchClient esClient = new ElasticsearchClient(transport);
+    }
+}
